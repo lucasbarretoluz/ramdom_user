026d07c8cddcdbdc1c7748732258974e0ef512db
lucasbarretoluz <lucasbarreto@hotmail.com>
2023-02-19 22:54:13 -0300

    add users model and request to api

--- lib/data/datasources/remote_datasource/remote_datasource.dart
+++ lib/data/datasources/remote_datasource/remote_datasource.dart
@@ -1,7 +1,32 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:ramdom_user/data/datasources/api.dart';
+import 'package:ramdom_user/data/model/user_model.dart';
 
 class RemoteDataSource {
   final http.Client client;
 
   RemoteDataSource(this.client);
+
+  Future<UserResponseModel> getUsers() async {
+    Uri url = Uri.parse(Api.apiUrl);
+
+    try {
+      http.Response response = await client.get(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      );
+      if (response.statusCode == 200) {
+        UserResponseModel modelResponse =
+            UserResponseModel.fromJson(json.decode(response.body));
+        return modelResponse;
+      }
+    } catch (e) {
+      throw Exception();
+    }
+
+    throw Exception();
+  }
 }
--- lib/data/model/user_model.dart
+++ lib/data/model/user_model.dart
@@ -1,5 +1,35 @@
-class UserModel {
-  UserModel({
+import 'package:ramdom_user/domain/entities/street_entity.dart';
+import 'package:ramdom_user/domain/entities/user_response_entity.dart';
+import 'dart:convert';
+
+import '../../domain/entities/image_entity.dart';
+import '../../domain/entities/user_entity.dart';
+
+class UserResponseModel extends UserResponseEntity {
+  const UserResponseModel({
+    required this.users,
+  }) : super(users: users);
+
+  final List<UserModel> users;
+
+  factory UserResponseModel.fromJson(Map<String, dynamic> json) =>
+      UserResponseModel(
+        users: List<UserModel>.from(
+            json['results'].map((x) => UserModel.fromJson(json))),
+      );
+}
+
+class UserModel extends UserEntity {
+  final String id;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String phone;
+  final ImageModel imageUrl;
+  final String gender;
+  final String nat;
+
+  const UserModel({
     required this.id,
     required this.firstName,
     required this.lastName,
@@ -7,40 +37,61 @@ class UserModel {
     required this.phone,
     required this.imageUrl,
     required this.gender,
-  });
+    required this.nat,
+  }) : super(
+          id: id,
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          phone: phone,
+          imageUrl: imageUrl,
+          gender: gender,
+          nat: nat,
+        );
 
-  String id;
-  String firstName;
-  String lastName;
-  String email;
-  String phone;
-  String imageUrl;
-  String gender;
-
-  String get fullname => this.firstName + " " + this.lastName;
-
-  UserModel.fromJson(Map<String, dynamic> json)
-      : id = json[UserKey.login][UserKey.uid] ?? "",
-        firstName = json[UserKey.name][UserKey.first] ?? "",
-        lastName = json[UserKey.name][UserKey.last] ?? "",
-        email = json[UserKey.email] ?? "",
-        phone = json[UserKey.phone] ?? "",
-        imageUrl = json[UserKey.picture][UserKey.large] ?? "",
-        gender = json[UserKey.gender] ?? "";
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        id: json['login']['uid'] ?? "",
+        firstName: json['login']['uid'] ?? "",
+        lastName: json['login']['uid'] ?? "",
+        email: json['email'],
+        phone: json['phone'],
+        imageUrl: ImageModel.fromJson(json['imageUrl']),
+        gender: json['gender'],
+        nat: json['nat'],
+      );
 }
 
-class UserKey {
-  static const login = "login";
-  static const uid = "uid";
-  static const name = "name";
-  static const email = "email";
-  static const first = "first";
-  static const last = "last";
-  static const phone = "phone";
-  static const picture = "picture";
-  static const medium = "medium";
-  static const large = "large";
-  static const gender = "gender";
+class StreetModel extends StreetEntity {
+  final String name;
+  final String number;
 
-  static const results = "results";
+  StreetModel({
+    required this.name,
+    required this.number,
+  }) : super(
+          name: name,
+          number: number,
+        );
+
+  factory StreetModel.fromJson(Map<String, dynamic> json) =>
+      StreetModel(name: json[''] ?? "", number: json[''] ?? "");
+}
+
+class ImageModel extends ImageEntity {
+  final String large;
+  final String medium;
+  final String thumbnail;
+
+  ImageModel({
+    required this.large,
+    required this.medium,
+    required this.thumbnail,
+  }) : super(
+          large: large,
+          medium: medium,
+          thumbnail: thumbnail,
+        );
+
+  factory ImageModel.fromJson(Map<String, dynamic> json) => ImageModel(
+      large: json[''] ?? "", medium: json[''] ?? "", thumbnail: json[''] ?? "");
 }
